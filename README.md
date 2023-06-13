a9dadd9b54ee02664a3206cc928e16b61282aaad
Bob Oyier <boboyier92@gmail.com>
2023-06-13 23:17:45 +0300

    Add User and Password regex for form validation

---
+++ README.md
@@ -0,0 +1,18 @@
+# FORM LOGIN, REGISTRATION AND AUTHORIZATION.
+
+Add user login and registration to your React app.
+Also learn how to apply role-based permissions and routing, and incorporate JWT access and refresh tokens for secure and continuous authentication.
+
+## Tutorial by Dave Gray.
+
+[FULL PLAYLIST HERE](https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PRCpm9clX0WiBEMB70FWwd)
+
+## Authors
+
+Project contributed to and maintained by:
+
+- [Bob Oyier](https://github.com/oyieroyier/)
+
+### ISC License (ISC)
+
+[![License: ICL](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useRef, useState, useEffect } from 'react';
+import { FaCheck, FaTimes, FaInfoCircle } from 'react-icons/fa';
+
+/*
+	USER REGEX rules:
+		1. Must start with a lowercase or uppercase letter.
+		2. Must be followed by 3 to 23 characters that can be lowercase letters, digits, hyphens or underscores
+		3. In hoer, it must be 4 to 24 characters.
+*/
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+
+/*
+	PASSWORD REGEX rules:
+		1. At least one lowercase letter
+		2. At least one uppercase letter.
+		3. At least one digit.
+		4. At least one special character.
+		5. The password itself can anywhere from 8 to 24 characters.
+*/
+const PWD_REGEX = /^(?=.*[a-z])(?=[A-Z])(?=.*[0-9])(?=.[!@#$%]).{8,24 }$/;
 
 const Register = () => {
-	return (
-		<div>Register</div>
-	)
-}
+	return <div>Register</div>;
+};
 
-export default Register
+export default Register;
